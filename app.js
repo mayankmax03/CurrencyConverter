571090961f788bbc7d1a3df75e8d19b4e0f9321e
Mayank Varshney <mayank.max02@gmail.com>
2025-04-20 17:53:21 -0700

    first commit

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ for (let select of dropdowns) {
     let newOption = document.createElement("option");
     newOption.innerText = currCode;
     newOption.value = currCode;
-    if (select.name === "from" && currCode === "USD") {
+    if (select.name === "from" && currCode === "PKR") {
       newOption.selected = "selected";
     } else if (select.name === "to" && currCode === "INR") {
       newOption.selected = "selected";
